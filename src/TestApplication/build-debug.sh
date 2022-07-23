e21e6754a790cd65ec67d9fc5c37b71d29a8109b
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2022-07-23 17:46:24 +0200

    Build scripts update

--- src/TestApplication/build-debug.sh
+++ src/TestApplication/build-debug.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
+#: Title       : build-debug.sh
+#: Date        : 2022-07-23
+#: Author      : Kjetil Kristoffer Solberg <post@ikjetil.no>
+#: Version     : 1.0
+#: Description : Builds TestApplication and TestDaemon.
 echo "Building TestApplication..."
-echo "> Using debug build <"
-
-#cd $HOME/Code/C++/Cpp.Include.Linux/src/TestApplication
+echo "> using debug build <"
 
 g++ -g TestApplication.cpp --std=c++17 -luuid -lpthread -o TestApplication
 if [[ $? -eq 0 ]]
 then
-    echo "> TestApplication build Ok <"
+    echo "> TestApplication build ok <"
 else
-    echo "> TestApplication build Error <"
+    echo "> TestApplication build error <"
 fi
+echo "> build process complete <"
 
+echo "Building TestDaemon ..."
+echo "> using debug build <"
 g++ -g TestDaemon.cpp --std=c++17 -o TestDaemon
 if [[ $? -eq 0 ]]
 then
-    echo "> TestDaemon build Ok <"
+    echo "> TestDaemon build ok <"
 else
-    echo "> TestDaemon build Error <"
+    echo "> TestDaemon build error <"
 fi
-
-echo "> Build process Complete <"
+echo "> build process complete <"
