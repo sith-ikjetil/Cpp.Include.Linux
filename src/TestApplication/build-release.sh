44440fc16b0805a4cc19be3abe229290ad736ed5
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2022-07-23 14:58:31 +0200

    Build scripts updated

--- src/TestApplication/build-release.sh
+++ src/TestApplication/build-release.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-cd $HOME/Code/C++/Cpp.Include.Linux/src/TestApplication
+echo "Building TestApplication..."
+echo "> Using debug build <"
+
+#cd $HOME/Code/C++/Cpp.Include.Linux/src/TestApplication
+
 g++ TestApplication.cpp --std=c++17 -luuid -lpthread -o TestApplication
+if [[ $? -eq 0 ]]
+then
+    echo "> TestApplication build Ok <"
+else
+    echo "> TestApplication build Error <"
+fi
+
 g++ TestDaemon.cpp --std=c++17 -o TestDaemon
+if [[ $? -eq 0 ]]
+then
+    echo "> TestDaemon build Ok <"
+else
+    echo "> TestDaemon build Error <"
+fi
+
+echo "> Build process Complete <"
